ed1241f978c6c859ded4c000839d9a2d7db15ac6
Fnu Tsering <fnu.tsering46@myhunter.cuny.edu>
2022-05-15 20:55:23 -0400

    Added score board and play button

--- TicTacToeUI.py
+++ TicTacToeUI.py
@@ -2,23 +2,28 @@ import PyQt5
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QFormLayout, QLineEdit
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import Qt
 
 class TicTacToeUI(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("TIC TAC TOE")
-        self.mainLayout = QVBoxLayout()
+        # self.setFixedSize(490, 700)
+        self.setGeometry(100, 100, 280, 80)
+        self.mainLayout = QHBoxLayout()
         self._centralWidget = QWidget(self)
         self._centralWidget.setLayout(self.mainLayout)
         self.setCentralWidget(self._centralWidget)
 
         self.createGrid()
+        self.createScoreBoard()
 
     def createGrid(self):
         gridLayout = QGridLayout()
@@ -26,11 +31,50 @@ class TicTacToeUI(QMainWindow):
         for row in range(3):
             for col in range(3):
                 button = QPushButton(f"{row}{col}")
-                button.setFixedSize(100, 100)
+                # button.setFixedSize(150, 150)
+                button.setMinimumSize(150, 150)
+                button.setMaximumSize(400, 400)
+                button.setStyleSheet("QPushButton {background-color: pink}")
+                button.setDisabled(True)
+
                 self.buttons[f"{row}{col}"] = button
                 gridLayout.addWidget(button, row, col)
-
+                gridLayout.setSpacing(20)
         self.mainLayout.addLayout(gridLayout)
+
+
+    def createScoreBoard(self):
+        self.rightLayout = QVBoxLayout()
+        self.scoreBoardLayout = QFormLayout()
+        self.playerScore = QLineEdit()
+        self.enemyScore = QLineEdit()
+
+        self.scoreBoardWidget = QWidget()
+        self.playerScore.setDisabled(True)
+        self.enemyScore.setDisabled(True)
+        self.playerScore.setFixedSize(80, 30)
+        self.playerScore.setPlaceholderText('0')
+        self.enemyScore.setPlaceholderText('0')
+        self.enemyScore.setFixedSize(80, 30)
+
+        self.scoreBoardLayout.addRow('Player X:',  self.playerScore)
+        self.scoreBoardLayout.addRow('Player O:',  self.enemyScore)
+        self.scoreBoardWidget.setLayout(self.scoreBoardLayout)
+
+
+        self.rightLayout.addWidget(self.scoreBoardWidget)
+        self.createPlayButton()
+        self.mainLayout.addLayout(self.rightLayout)
+
+
+    def createPlayButton(self):
+        self.playButton = QPushButton("NEW GAME")
+        self.playButton.setFixedSize(100, 50)
+        self.playButton.setStyleSheet("QPushButton {background-color: red}")
+        self.rightLayout.addSpacing(10)
+        self.rightLayout.addWidget(self.playButton, alignment=Qt.AlignCenter)
+
+
 
 
 
